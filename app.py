0093bdb1a534f68d2749a1c308a236d7a2dda648
DESKTOP-0LUN99P\parkm <parkmansoo@nate.com>
2022-03-08 16:13:36 +0900

    메인페이지 db 2

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-from pymongo import MongoClient
-client = MongoClient('3.35.220.165', 27017, username="test", password="test")
-db = client.mini_project
-
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://www.fuleaf.com/plants',headers=headers)
-
-
-
-
-
 
 
 soup = BeautifulSoup(data.text, 'html.parser')
@@ -25,9 +16,3 @@ for list in main_list:
     title = list.select_one('a > div.plant__title-flex > h3').text
 
     print(title, image)
-
-    doc = {
-        'title':title,
-        'image':image
-    }
-    db.mini_project.insert_one(doc)
