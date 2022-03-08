c6b27ce4d76d2bec0f3a6e562b5875c6f71620e0
hyopp <khb2100@gmail.com>
2022-03-08 18:11:19 +0900

    일단올림

---
+++ templates/app.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+from pymongo import MongoClient
+client = MongoClient('3.35.220.165', 27017, username="test", password="test")
+db = client.mini_project
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://www.fuleaf.com/plants',headers=headers)
+
+
+soup = BeautifulSoup(data.text, 'html.parser')
+
+main_list = soup.select('#plants_list > ul > div')
+
+
+for list in main_list:
+    plant_img = list.select_one('a > div.plant__image').get('style').replace('background-image: url(', '').replace(');', '')
+    image = plant_img
+    plant_name = list.select_one('a > div.plant__title-flex > h3').text
+
+    print(plant_name, image)
+
+    doc = {
+        'title':plant_name,
+        'image':image
+    }
+    db.mini_project.insert_one(doc)
