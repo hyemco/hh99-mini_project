a225270567ccf700cc7a26b9cd02797d92b76358
hyemco <hyemco@gmail.com>
2022-03-10 04:22:43 +0900

    로그인 완료/메인페이지 수정

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
 
-
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
@@ -16,18 +15,18 @@ SECRET_KEY = 'SPARTA'
 # 서버용 mongodb
 client = MongoClient('3.35.17.197', 27017, username="test", password="test")
 
-# 로컬용 mongodb
+# 로컬호스트용 mongodb
 # client = MongoClient('localhost', 27017)
 db = client.dbmini_project
 
-
 @app.route('/')
 def home():
+    plant_card = list(db.plants.find({}, {'_id':False}).limit(30))
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
 
-        return render_template('index.html')
+        return render_template('index.html', plant_card=plant_card)
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
@@ -40,9 +39,23 @@ def login():
     return render_template('login.html', msg=msg)
 
 
-# API - 로그인
+@app.route('/user/<username>')
+def user(username):
+    # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
+
+        user_info = db.users.find_one({"username": username}, {"_id": False})
+        return render_template('user.html', user_info=user_info, status=status)
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+
 @app.route('/sign_in', methods=['POST'])
 def sign_in():
+    # 로그인
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
 
@@ -51,8 +64,8 @@ def sign_in():
 
     if result is not None:
         payload = {
-         'id': username_receive,
-         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
 
@@ -62,22 +75,19 @@ def sign_in():
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
-# API - 회원가입
 @app.route('/sign_up/save', methods=['POST'])
 def sign_up():
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     doc = {
-        "username": username_receive,                               # 아이디
-        "password": password_hash,                                  # 비밀번호
-        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
+        "username": username_receive,  # 아이디
+        "password": password_hash,  # 비밀번호
     }
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
 
 
-# API - 아이디 중복확인
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
     username_receive = request.form['username_give']
@@ -92,6 +102,7 @@ def listing():
     for card in plant_card:
         card['_id'] = str(card['_id'])
     return jsonify({'plant_card': plant_card})
+
 
 
 # # 상세 페이지 크롤링
