3bc1c146d6f6d77a6bd04839ae19c2df7306f3f5
hyemco <hyemco@gmail.com>
2022-03-09 12:14:21 +0900

    로그인 페이지 분리 & 메인 페이지 식물 리스트 보이기

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from pymongo import MongoClient
 import jwt
 import datetime
 import hashlib
-
-
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
@@ -14,59 +12,44 @@ app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
 
-client = MongoClient('mongodb+srv://test:sparta@cluster0.n0n0a.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db = client.dbsparta_plus_week4
+# 서버용 mongodb
+# client = MongoClient('3.35.17.197', 27017, username="test", password="test")
 
-import requests
-from bs4 import BeautifulSoup
-
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://www.fuleaf.com/plants',headers=headers)
-
-
-soup = BeautifulSoup(data.text, 'html.parser')
-
-main_list = soup.select('#plants_list > ul > div')
-
-
-for list in main_list:
-    main_image = list.select_one('a > div.plant__image').get('style').replace('background-image: url(', '').replace(');', '')
-    image = main_image
-    title = list.select_one('a > div.plant__title-flex > h3').text
-
-    print(title, image)
+# 로컬호스트용 mongodb
+client = MongoClient('localhost', 27017)
+db = client.dbmini_project
 
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.users.find_one({"user_id": payload["id"]})
+        user_info = db.users.find_one({"username": payload["id"]})
         return render_template('index.html', user_info=user_info)
-
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
     except jwt.exceptions.DecodeError:
         return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
 
 
-# @app.route('/login')
-# def login():
-#     msg = request.args.get("msg")
-#     return render_template('login.html', msg=msg)
-
-@app.route('/login', methods=['POST'])
+@app.route('/login')
 def login():
-    # 로그인
-    user_id_receive = request.form['user_id_give']
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+
+# API - 로그인
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    username_receive = request.form['username_give']
     password_receive = request.form['password_give']
 
     pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    result = db.users.find_one({'user_id': user_id_receive, 'password': pw_hash})
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
 
     if result is not None:
         payload = {
-         'id': user_id_receive,
+         'id': username_receive,
          'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
@@ -77,75 +60,62 @@ def login():
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
+# API - 회원가입
 @app.route('/sign_up/save', methods=['POST'])
 def sign_up():
-    user_id_receive = request.form['user_id_give']
+    username_receive = request.form['username_give']
     password_receive = request.form['password_give']
-
-    # 비밀번호 encoding 후 저장(해쉬함수)
     password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     doc = {
-        "user_id": user_id_receive,                               # 아이디
+        "username": username_receive,                               # 아이디
         "password": password_hash,                                  # 비밀번호
-        "profile_name": user_id_receive,                           # 프로필 이름 기본값은 아이디
-        # "profile_pic": "",                                          # 프로필 사진 파일 이름
+        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
+        "profile_pic": "",                                          # 프로필 사진 파일 이름
         "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
-        # "profile_info": ""                                          # 프로필 한 마디
+        "profile_info": ""                                          # 프로필 한 마디
     }
-    db.usersinfo.insert_one(doc)
+    db.users.insert_one(doc)
     return jsonify({'result': 'success'})
 
 
+# API - 아이디 중복확인
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
-    user_id_receive = request.form['user_id_give']
-    exists = bool(db.users.find_one({"user_id": user_id_receive}))
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
     return jsonify({'result': 'success', 'exists': exists})
 
 
-# @app.route('/update_profile', methods=['POST'])
-# def save_img():
-#     token_receive = request.cookies.get('mytoken')
-#     try:
-#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#         # 프로필 업데이트
-#         return jsonify({"result": "success", 'msg': '프로필을 업데이트했습니다.'})
-#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-#         return redirect(url_for("home"))
-#
-#
-# @app.route('/posting', methods=['POST'])
-# def posting():
-#     token_receive = request.cookies.get('mytoken')
-#     try:
-#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#         # 포스팅하기
-#         return jsonify({"result": "success", 'msg': '포스팅 성공'})
-#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-#         return redirect(url_for("home"))
-#
-#
-# @app.route("/get_posts", methods=['GET'])
-# def get_posts():
-#     token_receive = request.cookies.get('mytoken')
-#     try:
-#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#         # 포스팅 목록 받아오기
-#         return jsonify({"result": "success", "msg": "포스팅을 가져왔습니다."})
-#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-#         return redirect(url_for("home"))
-#
-#
-# @app.route('/update_like', methods=['POST'])
-# def update_like():
-#     token_receive = request.cookies.get('mytoken')
-#     try:
-#         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-#         # 좋아요 수 변경
-#         return jsonify({"result": "success", 'msg': 'updated'})
-#     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-#         return redirect(url_for("home"))
+# API - DB 데이터 불러오기(메인페이지-식물 리스트)
+@app.route('/listing', methods=['GET'])
+def listing():
+    plant_card = list(db.plants.find({}).limit(30))
+    for card in plant_card:
+        card['_id'] = str(card['_id'])
+    return jsonify({'plant_card': plant_card})
 
+
+# # 상세 페이지 크롤링
+# import requests
+# from bs4 import BeautifulSoup
+#
+# headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+# data = requests.get('https://www.fuleaf.com/plants',headers=headers)
+#
+# soup = BeautifulSoup(data.text, 'html.parser')
+#
+# main_list = soup.select('#plants_list > ul > div')
+#
+# for list in main_list:
+#     main_image = list.select_one('a > div.plant__image').get('style').replace('background-image: url(', '').replace(');', '')
+#     image = main_image
+#     title = list.select_one('a > div.plant__title-flex > h3').text
+#     print(title, image)
+#     doc = {
+#         'title':title,
+#         'image':image
+#     }
+#     db.plants.insert_one(doc)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
