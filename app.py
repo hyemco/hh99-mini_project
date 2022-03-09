e16fcb13e67be73bcf1e415fe78b0736950e991e
hyopp <khb2100@gmail.com>
2022-03-10 03:53:56 +0900

    검색기능추가

---
+++ app.py
@@ -0,0 +1,131 @@
+from pymongo import MongoClient
+import jwt
+import datetime
+import hashlib
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
+
+# 서버용 mongodb
+client = MongoClient('3.35.17.197', 27017, username="test", password="test")
+
+# 로컬호스트용 mongodb
+# client = MongoClient('localhost', 27017)
+db = client.dbmini_project
+
+@app.route('/')
+def home():
+    plant_card = list(db.plants.find({}, {'_id':False}).limit(30))
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+
+        return render_template('index.html', plant_card=plant_card)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+
+@app.route('/user/<username>')
+def user(username):
+    # 각 사용자의 프로필과 글을 모아볼 수 있는 공간
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        status = (username == payload["id"])  # 내 프로필이면 True, 다른 사람 프로필 페이지면 False
+
+        user_info = db.users.find_one({"username": username}, {"_id": False})
+        return render_template('user.html', user_info=user_info, status=status)
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return redirect(url_for("home"))
+
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,  # 아이디
+        "password": password_hash,  # 비밀번호
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+
+# API - DB 데이터 불러오기(메인페이지-식물 리스트)
+@app.route('/listing', methods=['GET'])
+def listing():
+    plant_card = list(db.plants.find({}).limit(30))
+    for card in plant_card:
+        card['_id'] = str(card['_id'])
+    return jsonify({'plant_card': plant_card})
+
+
+
+# # 상세 페이지 크롤링
+# import requests
+# from bs4 import BeautifulSoup
+#
+# headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+# data = requests.get('https://www.fuleaf.com/plants',headers=headers)
+#
+# soup = BeautifulSoup(data.text, 'html.parser')
+#
+# main_list = soup.select('#plants_list > ul > div')
+#
+# for list in main_list:
+#     main_image = list.select_one('a > div.plant__image').get('style').replace('background-image: url(', '').replace(');', '')
+#     image = main_image
+#     title = list.select_one('a > div.plant__title-flex > h3').text
+#     print(title, image)
+#     doc = {
+#         'title':title,
+#         'image':image
+#     }
+#     db.plants.insert_one(doc)
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
